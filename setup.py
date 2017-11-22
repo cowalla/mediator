9b3f1390adb87be52a6922a526de518d55c3c952
cowalla <wallaconno@gmail.com>
2017-11-22 15:05:36 -0800

    add setup and gitignore

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "Mediator",
+    version = "0.0.1",
+    author = "Connor Wallace",
+    author_email = "wallaconno@gmail.com",
+    description = ('Client that allows communication between many exchanges'),
+    license = "MIT",
+    keywords = "cryptocurrency client",
+    url = "http://www.github.com/cowalla/mediator",
+    packages=['clients', 'fixtures'],
+    long_description=read('readme.md'),
+)
